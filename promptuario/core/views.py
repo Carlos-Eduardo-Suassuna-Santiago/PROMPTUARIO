eb18ed2262de6ac113bbca8849c1569ba1bc22d8
Eduardo Pereira Carlos <eduardocarlosport@gmail.com>
2025-11-14 19:53:22 -0300

    Atualização na estrutura de pastas do projeto

--- promptuario/core/views.py
+++ promptuario/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+    return render(request, 'core/login.html')
+
+def cadastroPaciente(request):
+    return render(request, 'core/cadastroPaciente.html')
+
