c13421602659307f4167999b3e608e30a0414b71
Carlos-Eduardo-Suassuna-Santiago <carloseduardosuassunasantiago@gmail.com>
2025-11-17 22:48:21 -0300

    Restruturação dos arquivos do projeto

--- promptuario/core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def login(request):
-    return render(request, 'core/login.html')
-
-def cadastro(request):
-    return render(request, 'core/cadastro.html')
-
