777ae7ed6057c3e85dd044d13a6303c992b9fd66
Douglas Lopes <douglaslopesq@gmail.com>
2026-01-27 23:21:05 -0300

    feature/implementação do relatório do admin

--- reports/urls.py
+++ reports/urls.py
@@ -5,6 +5,8 @@ app_name = 'reports'
 
 urlpatterns = [
     path('', views.ReportListView.as_view(), name='report_list'),
+    path('admin/', views.AdminReportView.as_view(), name='admin_report'),
+    path('admin/pdf/', views.AdminReportPDFView.as_view(), name='admin_report_pdf'),
     path('doctor/', views.DoctorReportView.as_view(), name='doctor_report'),
     path('generate/', views.ReportGenerateView.as_view(), name='report_generate'),
     path('pdf/', views.ReportPDFView.as_view(), name='report_pdf'),
